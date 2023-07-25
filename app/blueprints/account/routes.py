96dfee43156873fc30ea2e09339e51e37482aaf7
Gouldy-C <mail4clg@gmail.com>
2023-07-25 13:42:46 -0500

    7/25/23

--- app/blueprints/account/routes.py
+++ app/blueprints/account/routes.py
@@ -5,7 +5,7 @@ from . import bp as account
 from app import app
 from app.blueprints.account.forms import PokeDataForm
 from app.models import User, Pokedex, Team, TeamPokemon
-from app.blueprints.api.helper import get_dict_info_pokemon, get_all_pokemon_dict
+from app.blueprints.account.helper import get_dict_info_pokemon, get_all_pokemon_dict
 
 
 @app.before_request
@@ -54,22 +54,26 @@ def form_search():
 def add_to_pokedex():
     if g.poke_data_form.validate_on_submit():
         pokemon = g.poke_data_form.poke_name.data
-    try:
-        poke_info = get_dict_info_pokemon(pokemon)
-        pe = Pokedex()
-        pe.user_id = current_user.id
-        pe.name = poke_info['name']
-        pe.pokemon_id = poke_info['pokemon_id']
-        pe.height = poke_info['height']
-        pe.weight = poke_info['weight']
-        pe.base_exp = poke_info['base_exp']
-        pe.sprite = poke_info['sprite']
-        pe.types = ', '.join(poke_info['types'])
-        pe.pick_rand_moves(list(poke_info['moves'].keys()))
-        pe.commit()
-        flash(f'{pokemon.title()} has been added to your PokeDex.', category='success')
-        return redirect(url_for('account.home', username=current_user.username,search_form=g.search_form, poke_data_form=g.poke_data_form))
-    except:
+    if pokemon not in [poke.name for poke in current_user.pokedex]:
+        try:
+            poke_info = get_dict_info_pokemon(pokemon)
+            pe = Pokedex()
+            pe.user_id = current_user.id
+            pe.name = poke_info['name']
+            pe.pokemon_id = poke_info['pokemon_id']
+            pe.height = poke_info['height']
+            pe.weight = poke_info['weight']
+            pe.base_exp = poke_info['base_exp']
+            pe.sprite = poke_info['sprite']
+            pe.types = ', '.join(poke_info['types'])
+            pe.pick_rand_moves(list(poke_info['moves'].keys()))
+            pe.commit()
+            flash(f'{pokemon.title()} has been added to your PokeDex.', category='success')
+            return redirect(url_for('account.home', username=current_user.username,search_form=g.search_form, poke_data_form=g.poke_data_form))
+        except:
+            flash(f'{pokemon.title()} is already in your PokeDex or does not exist.', category='warning')
+            return redirect(url_for('account.home', username=current_user.username, search_form=g.search_form,  poke_data_form=g.poke_data_form))
+    else:
         flash(f'{pokemon.title()} is already in your PokeDex or does not exist.', category='warning')
         return redirect(url_for('account.home', username=current_user.username, search_form=g.search_form,  poke_data_form=g.poke_data_form))
     
--- app/blueprints/api/helper.py
+++ app/blueprints/api/helper.py
@@ -1,39 +1,7 @@
-import requests
 from app import app
 from flask import jsonify
 from flask_jwt_extended import get_jwt,create_access_token,get_jwt_identity
 from datetime import datetime, timezone, timedelta
-
-all_pokemon_dict = {}
-all_pokemon_info_dict = {}
-
-def get_all_pokemon_dict():
-    global all_pokemon_dict
-    if not all_pokemon_dict:
-        api_url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
-        response = requests.get(api_url).json()
-        for pokemon in response['results']:
-            all_pokemon_dict[pokemon['name']] = pokemon['url']
-    return all_pokemon_dict
-
-
-def get_dict_info_pokemon(name):
-    global all_pokemon_info_dict
-    if name not in all_pokemon_info_dict:
-        api_url = 'https://pokeapi.co/api/v2/pokemon/' + name
-        response = requests.get(api_url).json()
-        all_pokemon_info_dict[response['name']] = {
-            'name': response['name'],
-            'pokemon_id': response['id'],
-            'height': response['height'],
-            'weight': response['weight'],
-            'base_exp': response['base_experience'],
-            'sprite': response['sprites']['front_default'],
-            'moves': {move['move']['name']: move['move']['url'] for move in response['moves']},
-            'types': [type['type']['name'] for type in response['types']]
-        }
-    return all_pokemon_info_dict[name]
-
 
 @app.after_request
 def refresh_expiring_jwt(response):
